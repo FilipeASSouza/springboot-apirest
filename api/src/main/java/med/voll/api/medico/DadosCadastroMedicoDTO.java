c5b12e8626f22537f2124f8967db9b6e2dafa875
Filipe NoteCasa <filipe.sistemas@gmail.com>
2023-12-05 12:03:56 -0300

    add: mapeamento, dto, enum

---
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedicoDTO.java
@@ -0,0 +1,7 @@
+package med.voll.api.medico;
+
+import med.voll.api.endereco.DadosEnderecoDTO;
+
+public record DadosCadastroMedicoDTO(String nome, String email, String crm, Especialidade especialidade, DadosEnderecoDTO endereco) {
+
+}
