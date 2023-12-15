bf33da0ad9760b878e8125f753711d359368380b
Filipe NoteCasa <filipe.sistemas@gmail.com>
2023-12-15 09:55:37 -0300

    add: classe jpa usuarios, migration tabela usuarios

---
+++ api/src/main/java/med/voll/api/entidades/Usuarios.java
@@ -0,0 +1,26 @@
+package med.voll.api.entidades;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "usuarios")
+@Entity(name = "Usuarios")
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class Usuarios {
+	
+	@Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	private String login;
+	private String senha;
+
+}
