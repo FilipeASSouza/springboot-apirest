9d7099456bfaaefd6514a0177c1e2000f26923d3
Filipe NoteCasa <filipe.sistemas@gmail.com>
2023-12-15 10:07:04 -0300

    add: UsuariosRepository.java e classe AutenticacaoService.java. Arquivo de dicas consultar utilizando spring data

---
+++ Dicas spring data.md
@@ -0,0 +1,11 @@
+## Dicas ##
+
+Conforme aprendido em vídeos anteriores, o Spring Data utiliza um padrão próprio de nomenclatura de métodos que devemos seguir para que ele consiga gerar as queries SQL de maneira correta.
+
+Existem algumas palavras reservadas que devemos utilizar nos nomes dos métodos, como o findBy e o existsBy, para indicar ao Spring Data como ele deve montar a consulta que desejamos. Esse recurso é bastante flexível, podendo ser um pouco complexo devido às diversas possibilidades existentes.
+
+Para conhecer mais detalhes e entender melhor como montar consultas dinâmicas com o Spring Data, acesse a sua documentação oficial.
+
+## Documentacao oficial ##
+
+https://docs.spring.io/spring-data/jpa/reference/jpa/query-methods.html
---
+++ api/src/main/java/med/voll/api/infra/AutenticacaoService.java
@@ -0,0 +1,25 @@
+package med.voll.api.infra;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import med.voll.api.repository.UsuariosRepository;
+
+@Service
+public class AutenticacaoService implements UserDetailsService {
+
+	@Autowired//anotacao utilizada para fazer a injecao de dependencia
+	private UsuariosRepository repository;
+	
+	//classe sera identificada pelo spring no momento da autenticacao
+	
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+		
+		return repository.findByLogin(username);
+	}
+
+}
